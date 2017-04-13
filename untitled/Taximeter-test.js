8beb30d4c4e80132027fec95b856bc4d9b250a32
SmallWirldwind <390812516@qq.com>
2017-04-13 21:49:54 +0800

    complete:calculate fare test

--- untitled/Taximeter-test.js
+++ untitled/Taximeter-test.js
@@ -3,9 +3,16 @@
  */
 'use strict';
 describe("test calculate stop waiting time Cost" , function() {
-    it("return money when input > 0", function () {
+    it("return money when input >= 0", function () {
         const stopTime = 8;
-        const waitCost = calculateParkingCost(stopTime)
+        const waitCost = calculateParkingCost(stopTime);
         expect(waitCost).toBe(8*0.25);
     })
 });
+describe("test calculate Fare" , function () {
+    it("return money when input distance",function () {
+        const distance = 10;
+        const fare = calculateFare(distance);
+        expect(fare).toBe(6*0.8+2*1.2+6);
+    })
+})
