a3024e86cc425a0e627c77c1afeb5169645efb81
SmallWirldwind <390812516@qq.com>
2017-04-13 21:39:41 +0800

    complete:Taximeter-test when taxi parking Cost

--- untitled/Taximeter-test.js
+++ untitled/Taximeter-test.js
@@ -2,4 +2,10 @@
  * Created by haoyuze on 2017/4/13.
  */
 'use strict';
-describe("")
+describe("test calculate stop waiting time Cost" , function() {
+    it("return money when input > 0", function () {
+        const stopTime = 8;
+        const waitCost = calculateParkingCost(stopTime)
+        expect(waitCost).toBe(8*0.25);
+    })
+});
