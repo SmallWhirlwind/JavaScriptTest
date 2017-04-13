d9472e75457e37b2037fd0fc0007fd8648bd3d72
SmallWirldwind <390812516@qq.com>
2017-04-13 22:04:13 +0800

    complete:calculate Fare when input distance

--- untitled/Taximeter.js
+++ untitled/Taximeter.js
@@ -5,5 +5,12 @@ function calculateParkingCost(stopTime) {
     return stopTime*0.25;
 }
 function calculateFare(distance) {
-    if()
+    if(distance<=2){
+        return 6;
+    }
+    if(distance<=8){
+        return (distance-2)*0.8+6;
+    }else {
+        return (distance-8)*1.2+6*0.8+6;
+    }
 }
